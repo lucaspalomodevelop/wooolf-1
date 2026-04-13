803d773f12e7487289747e9ab521326c04a2950e
ck72 <c.klung@ostfalia.de>
2026-04-13 13:49:35 +0200

    comments

--- backend/src/main/java/questionCards.java
+++ backend/src/main/java/questionCards.java
@@ -1,10 +1,16 @@
+/**
+ * Stellt eine Fragekarte im Spiel dar.
+ * Jede Karte enthält genau eine Frage.
+ */
 public class questionCard {
     private final String question;
 
+    //Erstellt die Fragekarten mit den bestimmten Fragen
     public QuestionCard(String question) {
         this.question = question;
     }
 
+    //Gibt die Frage der Fragekarte zurueck
     public String getQuestion() {
         return question;
     }
--- backend/src/main/java/questionCardsDeck.java
+++ backend/src/main/java/questionCardsDeck.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class questionCardDeck {
-
+    //Strings fuer die verschiedenen Fragen
     private static final String Question1 = "Spielst du einen der Charaktere Wolf, Jäger oder Schäfer?";
     private static final String Question2 = "Spielst du einen der Charaktere Schaf, Jäger oder Schäfer?";
     private static final String Question3 = "Hat mindestens eine Deiner Charakterkarten das Charakterbild Jagdhund?";
@@ -11,6 +11,7 @@ public class questionCardDeck {
 
     private final List<QuestionCard> cards;
 
+    //Enum fuer alle Fragekarten. Anzahl der verschiedenen Fragen definiert
     public QuestionCardDeck() {
         cards = new ArrayList<>();
         for (int i = 0; i < 4; i++) cards.add(new QuestionCard(Question1));
@@ -20,6 +21,7 @@ public class questionCardDeck {
         for (int i = 0; i < 2; i++) cards.add(new QuestionCard(Question5));
     }
 
+    //getter fuer Cards
     public List<QuestionCard> getCards() {
         return cards;
     }
