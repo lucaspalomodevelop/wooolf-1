2f04e7ec5cb833a28ea248c642617652cf7e2047
ck72 <c.klung@ostfalia.de>
2026-04-13 12:46:52 +0200

    add class for questioncards and enum for every card

---
+++ backend/src/main/java/questionCards.java
@@ -0,0 +1,11 @@
+public class questionCard {
+    private final String question;
+
+    public QuestionCard(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+}
---
+++ backend/src/main/java/questionCardsDeck.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class questionCardDeck {
+
+    private static final String Question1 = "Spielst du einen der Charaktere Wolf, Jäger oder Schäfer?";
+    private static final String Question2 = "Spielst du einen der Charaktere Schaf, Jäger oder Schäfer?";
+    private static final String Question3 = "Hat mindestens eine Deiner Charakterkarten das Charakterbild Jagdhund?";
+    private static final String Question4 = "Hat mindestens eine Deiner Charakterkarten das Charakterbild Jäger?";
+    private static final String Question5 = "Hat mindestens eine Deiner Charakterkarten das Charakterbild Schaf?";
+
+    private final List<QuestionCard> cards;
+
+    public QuestionCardDeck() {
+        cards = new ArrayList<>();
+        for (int i = 0; i < 4; i++) cards.add(new QuestionCard(Question1));
+        for (int i = 0; i < 4; i++) cards.add(new QuestionCard(Question2));
+        for (int i = 0; i < 3; i++) cards.add(new QuestionCard(Question3));
+        for (int i = 0; i < 3; i++) cards.add(new QuestionCard(Question4));
+        for (int i = 0; i < 2; i++) cards.add(new QuestionCard(Question5));
+    }
+
+    public List<QuestionCard> getCards() {
+        return cards;
+    }
+
+}
