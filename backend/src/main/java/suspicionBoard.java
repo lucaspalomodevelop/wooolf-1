258b4f884dee5d846333a4f452de4ebc15d59f19
Philipp <p.ehrhardt@ostfalia.de>
2026-04-14 10:48:57 +0200

    #3 Modellierung der Verdächtigungstafel

---
+++ backend/src/main/java/suspicionBoard.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Repräsentiert die persönliche Verdächtigungstafel eines Spielers.
+ * Symbol X (keine Vermutung) = -1
+ */
+
+public class suspicionBoard {
+
+    private int firstSuspicion; /*Erste Verdächtigung */
+    private int secondSuspicion; /* Zweite Verdächtigung */
+    private character character; /* Referenz zum Charakter*/ 
+    
+    
+    /* Erstellung einer neuen Verdächtigungstafel für einen Spieler (Konstruktor) 
+    @param character: Der Charakter des Spielers, dessen wahre Identität für die Wolf-Sonderregel benötigt wird
+    */
+    public suspicionBoard(character character) {
+        this.character = character;
+        this.firstSuspicion = -1;
+        this.secondSuspicion = -1;
+    }
+    
+
+    /* Zurücksetzen der Verdächtigungstafel vor jeder Runde */
+    public void resetSuspicion(character character) {
+        this.firstSuspicion = -1;
+        this.secondSuspicion = -1;
+        this.character = character;
+    }
+
+
+    /* Setzt die beiden Verdächtigungszeiger
+    @param firstSuspicion: Index der ersten Vermutung (0-8 oder -1 für keine Vermutung)
+    @param secondSuspicion: Index der zweiten Vermutung (0-8 oder -1; nur, wenn der Charakter nicht "Wolf" ist)
+    */
+    public void setSuspicion(int firstSuspicion, int secondSuspicion) {
+        this.firstSuspicion = firstSuspicion;
+        if (!character.getTrueIdentity().equals("Wolf")) {
+            this.secondSuspicion = secondSuspicion;
+        } else {
+            this.secondSuspicion = -1; // Keine zweite Vermutung für "Wolf"
+        }
+    }
+
+    
+    /* Gibt die erste Verdächtigung zurück
+    @return: Index der ersten Vermutung (0-8 oder -1 für keine Vermutung)
+     */
+    public int getFirstSuspicion() {
+        return firstSuspicion;
+    }
+
+    
+    /* Gibt die zweite Verdächtigung zurück
+    @return: Index der zweiten Vermutung (0-8 oder -1 für keine Vermutung)
+     */
+    public int getSecondSuspicion() {
+        return secondSuspicion;
+    }
+
+}
